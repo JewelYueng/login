c9fbe8fff19974fd1c59aaeb600a2fed40e6f818
JewelYueng <414608192@qq.com>
2017-07-07 21:36:20 +0800

    改了一下返回吗的验证

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -162,7 +162,7 @@ export default {
               "Content-Type": 'application/json'
             }
           }).then(res => {
-            if(res.data.code === 200) {
+            if(res.data.code === "200") {
               this.$message({
                 message: '登陆成功',
                 type: 'success'
